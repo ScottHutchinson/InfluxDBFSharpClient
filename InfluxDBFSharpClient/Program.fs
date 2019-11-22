702898b1244db32567f128847df2e3004e80bc17
Scott Hutchinson <scotthutchinson@usa.net>
2019-11-22 15:53:14 -0800

    simplifying add fields

--- InfluxDBFSharpClient/Program.fs
+++ InfluxDBFSharpClient/Program.fs
@@ -4,7 +4,8 @@
 //let events = ConcurrentQueue<IInfluxDatapoint>()
 let event1 = InfluxDatapoint<'T>()
 let field1 = InfluxValueField(42.99) // :> IComparable<InfluxValueField>
-do event1.Fields.["amountRequestedUSD"] <- field1 // //Error	FS0001	The type 'InfluxValueField' is not compatible with the type 'IComparable<InfluxValueField>'.
+let addFields () = 
+    event1.Fields.Add("amountRequestedUSD", field1) // //Error	FS0001	The type 'InfluxValueField' is not compatible with the type 'IComparable<InfluxValueField>'.
 //let event2 = InfluxDatapoint<double>()
 //do events.Enqueue(event1)
 //do events.Enqueue(event2)
